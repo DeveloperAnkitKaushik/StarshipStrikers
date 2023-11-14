66d87335526e0f123a18cf352423b4376016353d
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-11-14 23:01:28 +0530

    minor changes

--- restriction.js
+++ restriction.js
@@ -2,28 +2,24 @@ document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keydown', function (event) { 
+  if (event.key === 'F12' || event.keyCode === 123) {
+    event.preventDefault();
+}
+  if (event.keyCode == 123) {
+    return false; 
+  }
+
+  if (event.key === 'i' || event.keyCode === 73) {
+    event.preventDefault();
+  }
+
   if (event.key === 'c' || event.keyCode === 67) {
     event.preventDefault();
   }
-  if (event.keyCode == 123) {
-    return false;
-  }
 
-  if (e.ctrlKey && e.shiftkey && e.keyCode == "I".charCodeAt(0)) {
-    return false;
-  }
-
-  if (e.ctrlKey && e.shiftkey && e.keyCode == "C".charCodeAt(0)) {
-    return false;
-  }
-
-  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-    return false;
-  }
-
-  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-    return false;
+  if (event.key === 'j' || event.keyCode === 74) {
+    event.preventDefault();
   }
 });
 
